48857fc5889de772f33a2194c727e0351802063b
x <x>
2020-12-05 13:57:48 +0800

    Main

---
+++ test.cpp
@@ -0,0 +1,85 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <regex>
+
+#include "TrieTree.h"
+using namespace std;
+
+void test1() {
+    // ac自动机 多模式串匹配
+    AC_automaton<char, false> aca;
+    vector<string> vs = {"aaa", "abcab"};
+    vector<string> ss{"bcd", "ce", "ce", "ababa", "abce"};
+
+    aca.buildTrieTree(ss);
+    aca.buildAC_automaton();
+    aca.start_ac_automaton("abcexbcdxxxcebcdbceabcece");
+}
+
+void test2() {
+    // 整数序列
+    TrieTree<int, true, 0> trie;
+    trie.insert({1, 2, 4, 7, 6, 6, 1});
+    trie.insert({200, 10, 21, 1293, 1, 53, 2});
+    trie.insert({1, 2, 4, 5, 6, 6, 1});
+    trie.insert({1, 3, 4, 1, 6, 6, 1});
+    for (auto i : trie) {
+        for (auto j : i) cout << j << " ";
+        cout << endl;
+    }
+    cout << trie.count({1, 2, 4, 5, 6, 6, 1}) << endl;
+    trie.erase({1, 2, 4, 5, 6, 6, 1});
+    auto y = trie.prefixWords({1, 2, 4});
+    for (auto yy : y) {
+        copy(yy.begin(), yy.end(), ostream_iterator<int>(cout, " "));
+    }
+}
+
+auto regex_matche_word(const string &s) {
+    regex rgx("(\\w+)");
+    sregex_iterator first = sregex_iterator{s.begin(), s.end(), rgx};
+    sregex_iterator last;
+    vector<string> words;
+    while (first != last) {
+        words.push_back(first->str(1));
+        first++;
+    }
+    return words;
+}
+
+void test3() {
+    // 词频统计
+    ifstream ifs("text.txt");
+    string s;
+    char c;
+    while (~(c = ifs.get())) s += c;
+    ifs.close();
+
+    TrieTree<> trie;
+    auto words = regex_matche_word(s);
+    unordered_map<string, int> cnt;
+    for (auto word : words) {
+        trie.insert(word);
+    }
+    trie.insert({'1', '2', '3'});
+    for (auto word : words) {
+        cnt[word] = trie.count(word);
+    }
+    for (auto [k, v] : cnt) {
+        cout << k << ":" << v << endl;
+    }
+    string the = "the";
+    trie.erase(the);
+    cout << trie.count("the") << endl;
+    // for (auto xx : trie) cout << xx << " ";
+    for (auto it = trie.begin(); it != trie.end(); ++it) cout << *it << " ";
+}
+int main() {
+    test3();
+    cout << endl;
+    test1();
+    cout << endl;
+    test2();
+    return 0;
+}
